a8bcc073b192739749f84d215bf295682e7e70dc
Tim Statler <62028706+timstats91@users.noreply.github.com>
2022-06-10 14:22:19 -0500

    Fetch Hook for Movie Data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <HeroComponent />
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        'https://api.themoviedb.org/3/movie/550?api_key=d4be5df613286799989a0a84c536f851&language=en-US&page=1'
+      )
+      const result = await data
+      console.log(result.data)
+    }
+  },
 }
 </script>

249571c2e48ec32de7e5d78077db0a5d37b2c933
Tim Statler <62028706+timstats91@users.noreply.github.com>
2022-06-13 14:22:57 -0500

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
+
+    <!-- Hero -->
     <HeroComponent />
+
+    <!-- Movies -->
+    <div class="container movies">
+      <div id="user-grid" class="movie-grid">
+        <div class="movie" v-for="(movie, index) in movies" :key="index">
+          <div class="movie-img">
+            <img :src="`https://tmdb.org/t/p/${movie.poster_path}`" alt="">
+            <p class="review">{{movie.vote_average}}</p>
+            <p class="overview">{{movie.overview}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,8 +36,11 @@ export default {
         'https://api.themoviedb.org/3/movie/550?api_key=d4be5df613286799989a0a84c536f851&language=en-US&page=1'
       )
       const result = await data
-      console.log(result.data)
-    }
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      console.log(this.movies)
+    },
   },
 }
 </script>
